f98641194acf7eaf1940858eba2324536b63c457
wasfijaradat <wasfijar@gmail.com>
2021-02-09 22:11:34 +0200

    guessing game

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Code-201-proj
+
+- This is a gussing game to evaluate the user trend and readablity to join a programming course. 
+- 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 let userInput1 = prompt('Are you a degree holder?');
-userInput1 = userInput1.toLowerCase(); //to validate the uppercase entry 
+userInput1 = userInput1.toLowerCase(); //to convert every entry to lower case 
 if (userInput1 === 'y' || userInput1 === 'yes') /*the user entry is true*/ {
 
     alert('that is great basically');
@@ -41,7 +41,7 @@ else if (userInput3 === 'no' || userInput3 === 'no') {
 else {
     alert('incorrect entry');
 }
-// console.log(userInput3); 
+console.log(userInput3);
 
 let userInput4 = prompt('Do you have a laptop?');
 userInput4 = userInput4.toLowerCase();
@@ -69,8 +69,87 @@ else {
     alert('incorrect entry');
 }
 
-// console.log(userInput5); 
+let score6=0;
+for (let i = 0; i < 4; i++) {
+    let userInput6 = prompt('Guess the no of courses we offer?');
+    userInput6 = parseInt(userInput6);
 
-let userInput6 = prompt('Enter your name');
-console.log(userInput6);
-alert(`WELCOME ${userInput6}`);
+
+    if (userInput6 === 10) {
+        alert('great guess! it took you ' + i + ' trials');
+        console.log(i);
+        break
+
+    }
+
+    else if (userInput6 < 10) {
+        alert('this is lower than what we offer');
+    }
+    else if (userInput6 > 10) {
+        alert('this is higher than what we offer');
+    }
+
+    else {
+        alert('wrong entry');
+    }
+    score6++
+}
+alert('we offer 10 programming courses');
+
+/*trial no 1 before the TA helped me out with thw for looping way*/
+
+// let userInput7=prompt('Guess one of our to top ranked courses');
+// let arrayAnsw=['html', 'javascript', 'java', 'php'];
+// userInput7=userInput7.toLowerCase();
+// let index;
+
+// for(index=0; index<6; index++){
+
+
+
+//     if (userInput7===arrayAnsw[0] || userInput7===arrayAnsw[1] || userInput7===arrayAnsw[2] || userInput7===arrayAnsw[3]){
+
+//         alert('great guess! it took you '+ index + ' trials');
+//         document.write(index + 'trials for question 7');
+//         console.log(index);
+//         break;
+//     }
+
+
+//     else {
+//        alert('you could not guess one of them correctly, Thank you'); 
+//        userInput7=prompt('Guess one of our to top ranked courses');
+//        userInput7=userInput7.toLowerCase();
+//     }
+// }
+
+
+/*trials to have guessing trials without using if statement, TA Hidaya demo*/
+
+let arrayAnsw = ['html', 'javascript', 'java', 'php']
+let falsy = false;
+
+for (let i = 0; i < 6; i++) {
+    let userInput7 = prompt('Guess one of our top ranked courses');
+    userInput7 = userInput7.toLowerCase();
+
+    for (let j = 0; j < arrayAnsw.length; j++) {
+        if (userInput7 === arrayAnsw[j]) {
+            falsy = true
+          
+            score6++
+            alert('good guess! you took ' + score + ' to guess');
+            break
+        }
+    }
+    if (falsy === true) {
+        break
+    }
+}
+
+
+let userInput8 = prompt('Enter your name');
+console.log(userInput8);
+alert(`WELCOME ${userInput8}`);
+
+
